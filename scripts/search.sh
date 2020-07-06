a6bd1cc2c089a256b83c67c5f2c094ba087b3d94
el <erampono@gmail.com>
2020-07-06 08:45:53 +0800

    url-encode queries

--- scripts/search.sh
+++ scripts/search.sh
@@ -1,5 +1,5 @@
 QUERY=$1
 URL='https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts|info&generator=search&inprop=url&exsentences=1&exlimit=max&exintro&explaintext&redirects'
-RESULTS=$(curl --silent "$URL&gsrsearch=$QUERY")
+RESULTS=$(curl --silent --data-urlencode "gsrsearch=$QUERY" $URL)
 echo $RESULTS | jq '.query.pages'
 
