471d3463fc7f8e5ad6a5af6a727ebb7d52ba2ba0
el <erampono@gmail.com>
2020-07-12 12:56:38 +0800

    use data-urlencode in curl

--- scripts/get_page.sh
+++ scripts/get_page.sh
@@ -1,7 +1,7 @@
 PAGE_ID="$1"
 URL='https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&explaintext'
 
-curl --silent "$URL&pageids=$PAGE_ID" \
+curl --silent --data-urlencode "pageids=$PAGE_ID" $URL \
   | jq ".query.pages" \
   | jq --arg pageid "$PAGE_ID" 'to_entries[] | select(.key==$pageid) | .value' \
   | tr -d '\000-\011\013\014\016-\037' \
